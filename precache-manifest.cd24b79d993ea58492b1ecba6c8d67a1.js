2f4e371e279a2520b4a4dfbf3dea5bfbc7267776
Ushkalovk <58294215+Ushkalovk@users.noreply.github.com>
2020-06-18 19:54:29 +0300

    Updates

---
+++ precache-manifest.cd24b79d993ea58492b1ecba6c8d67a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee49624b760ea12b74ab69f000ec2d7d",
+    "url": "/nasa/index.html"
+  },
+  {
+    "revision": "330eeacd307427327806",
+    "url": "/nasa/static/css/2.5036aa7d.chunk.css"
+  },
+  {
+    "revision": "178d0af635513f26d836",
+    "url": "/nasa/static/css/main.f0248dcb.chunk.css"
+  },
+  {
+    "revision": "330eeacd307427327806",
+    "url": "/nasa/static/js/2.43cbc67d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nasa/static/js/2.43cbc67d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "178d0af635513f26d836",
+    "url": "/nasa/static/js/main.a498ad44.chunk.js"
+  },
+  {
+    "revision": "649df1e927f3ecb3dd5c",
+    "url": "/nasa/static/js/runtime-main.30ff7636.js"
+  }
+]);
